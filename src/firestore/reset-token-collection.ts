742f3a44d4f06194c6acfabca5fdea1dce6d49a6
jake hamilton <jacobwilsonhamilton@gmail.com>
2023-02-05 21:35:22 -0600

    dfd

--- src/firestore/reset-token-collection.ts
+++ src/firestore/reset-token-collection.ts
@@ -1,129 +1,129 @@
-import { firestore, } from './firestore';
-import fs from 'node:fs';
-import { setDoc, type DocumentData, type DocumentReference, type QueryDocumentSnapshot, deleteDoc, where, QuerySnapshot } from 'firebase/firestore';
-import type { Order, Token, UserModel } from '@/models/user.model';
+// import { firestore, } from './firestore';
+// import fs from 'node:fs';
+// import { setDoc, type DocumentData, type DocumentReference, type QueryDocumentSnapshot, deleteDoc, where, QuerySnapshot } from 'firebase/firestore';
+// import type { Order, Token, UserModel } from '@/models/user.model';
 
 
 
-const { doc, getDocs, collection, runTransaction, query } = firestore;
+// const { doc, getDocs, collection, runTransaction, query } = firestore;
 
 
 
 
-// const tokenDocs = (await getDocs(tokenCollection))
+// // const tokenDocs = (await getDocs(tokenCollection))
 
 
 
-const tokenCollection = collection('tokens')
+// const tokenCollection = collection('tokens')
 
-const unownedTokenDocQuery = query(tokenCollection, where('owner', '==', null))
+// const unownedTokenDocQuery = query(tokenCollection, where('owner', '==', null))
 
-const tokenSnap = await getDocs(unownedTokenDocQuery);
+// const tokenSnap = await getDocs(unownedTokenDocQuery);
 
-// tokenSnap.docs.map(_ => _.)
-// tokenSnap.forEach(async (token: QueryDocumentSnapshot<unknown>): Promise<void> => {
-//   const tokenData = token.data() as Token;
+// // tokenSnap.docs.map(_ => _.)
+// // tokenSnap.forEach(async (token: QueryDocumentSnapshot<unknown>): Promise<void> => {
+// //   const tokenData = token.data() as Token;
 
-//   // doc.data() is never undefined for query doc snapshots
-//   const newTokenDoc = {
-//     ...tokenData,
-//     id: tokenData.id.toString(),
+// //   // doc.data() is never undefined for query doc snapshots
+// //   const newTokenDoc = {
+// //     ...tokenData,
+// //     id: tokenData.id.toString(),
+// //     modified: firestore.Timestamp.now(),
+// //   } as Token;
+
+// //   console.log(token.id, " => ", token.data());
+// // });
+
+
+// // try {
+// //   const updatedTokens = await runTransaction(async (transaction) => {
+
+// //     tokenSnap
+// //       .forEach(async (token: QueryDocumentSnapshot<unknown>): Promise<void> => {
+// //         const tokenDoc = await transaction.get(token.ref)
+// //         const tokenData = tokenDoc.data() as Token;
+
+// //         transaction.update(token.ref, {
+// //           ...tokenData,
+// //           id: tokenData.id.toString(),
+// //           modified: firestore.Timestamp.now(),
+// //         });
+
+// //         console.log(token.id, " => ", token.data());
+// //       });
+// //   });
+
+// // } catch (e) {
+// //   // This will be a "population is too big" error.
+// //   console.error(e);
+// // }
+
+
+
+// tokenSnap.forEach(async (token: any) => {
+// //   const stringId = token.id.toString()
+// //   //     console.error(e);
+//   await setDoc(doc('tokens', stringId), {
+//     owner: token.owner || null,
+//     id: stringId,
 //     modified: firestore.Timestamp.now(),
-//   } as Token;
+//   }, { merge: true });
 
-//   console.log(token.id, " => ", token.data());
+// //   console.warn('just ran', { stringId });
+
+// //   // (token.orders || []).forEach(async (order: any) => {
+// //   //   await setDoc(doc('tokens', token.wallet, 'orders', order.tokenId), { ...order }, { merge: true });
+// //   // });
 // });
 
 
-// try {
-//   const updatedTokens = await runTransaction(async (transaction) => {
+// //   try {
+// //     const newPopulation = await runTransaction(async (transaction) => {
+// //       const sfDoc = await transaction.get(sfDocRef);
+// //       if (!sfDoc.exists()) {
+// //         throw "Document does not exist!";
+// //       }
 
-//     tokenSnap
-//       .forEach(async (token: QueryDocumentSnapshot<unknown>): Promise<void> => {
-//         const tokenDoc = await transaction.get(token.ref)
-//         const tokenData = tokenDoc.data() as Token;
+// //       const newPop = sfDoc.data().population + 1;
+// //       if (newPop <= 1000000) {
+// //         transaction.update(sfDocRef, { population: newPop });
+// //         return newPop;
+// //       } else {
+// //         return Promise.reject("Sorry! Population is too big");
+// //       }
+// //     });
 
-//         transaction.update(token.ref, {
-//           ...tokenData,
-//           id: tokenData.id.toString(),
-//           modified: firestore.Timestamp.now(),
-//         });
-
-//         console.log(token.id, " => ", token.data());
-//       });
-//   });
-
-// } catch (e) {
-//   // This will be a "population is too big" error.
-//   console.error(e);
-// }
-
-
-
-tokenSnap.forEach(async (token: any) => {
-//   const stringId = token.id.toString()
-//   //     console.error(e);
-  await setDoc(doc('tokens', stringId), {
-    owner: token.owner || null,
-    id: stringId,
-    modified: firestore.Timestamp.now(),
-  }, { merge: true });
-
-//   console.warn('just ran', { stringId });
-
-//   // (token.orders || []).forEach(async (order: any) => {
-//   //   await setDoc(doc('tokens', token.wallet, 'orders', order.tokenId), { ...order }, { merge: true });
-//   // });
-});
+// //     console.log("Population increased to ", newPopulation);
+// //   } catch (e) {
+// //     // This will be a "population is too big" error.
+// //     console.error(e);
+// //   }
+// // });
 
 
-//   try {
-//     const newPopulation = await runTransaction(async (transaction) => {
-//       const sfDoc = await transaction.get(sfDocRef);
-//       if (!sfDoc.exists()) {
-//         throw "Document does not exist!";
-//       }
+// // tokenDocs.docs.forEach(async (user) => {
+// //   const normalizedWallet = user.id.toLowerCase();
+// //   console.warn({ tokenDocs });
 
-//       const newPop = sfDoc.data().population + 1;
-//       if (newPop <= 1000000) {
-//         transaction.update(sfDocRef, { population: newPop });
-//         return newPop;
-//       } else {
-//         return Promise.reject("Sorry! Population is too big");
-//       }
-//     });
+// //   const newUser = {
+// //     wallet: normalizedWallet,
+// //     orders: (await getDocs(collection('users', user.id, 'orders'))).docs.map(order => order.data() as Order)
+// //   }
 
-//     console.log("Population increased to ", newPopulation);
-//   } catch (e) {
-//     // This will be a "population is too big" error.
-//     console.error(e);
-//   }
-// });
+// //   console.warn({ newUser });
+// //   await setDoc(doc('users', normalizedWallet), { merge: true });
 
+// //   newUser.orders.forEach(async (order) => {
+// //     await setDoc(doc('users', normalizedWallet, 'orders', (order?.tokenId || '')), { merge: true });
 
-// tokenDocs.docs.forEach(async (user) => {
-//   const normalizedWallet = user.id.toLowerCase();
-//   console.warn({ tokenDocs });
+// //     // await deleteDoc(doc('users', user.id, 'orders', (order?.tokenId || '')));
+// //   });
 
-//   const newUser = {
-//     wallet: normalizedWallet,
-//     orders: (await getDocs(collection('users', user.id, 'orders'))).docs.map(order => order.data() as Order)
-//   }
+// //   // await deleteDoc(doc('users', user.id));
 
-//   console.warn({ newUser });
-//   await setDoc(doc('users', normalizedWallet), { merge: true });
+// //   console.warn('END OF USER DOCS FOR EACH');
+// // });
 
-//   newUser.orders.forEach(async (order) => {
-//     await setDoc(doc('users', normalizedWallet, 'orders', (order?.tokenId || '')), { merge: true });
-
-//     // await deleteDoc(doc('users', user.id, 'orders', (order?.tokenId || '')));
-//   });
-
-//   // await deleteDoc(doc('users', user.id));
-
-//   console.warn('END OF USER DOCS FOR EACH');
-// });
-
-export const butthole = 'fuk'
+// export const butthole = 'fuk'
 
 
