c6e45248d5a5dad97ef50fab06c2e30936b92c64
jake hamilton <jacobwilsonhamilton@gmail.com>
2023-07-01 02:10:20 -0500

    added end-game code

---
+++ src/firestore/firestore.ts
@@ -0,0 +1,61 @@
+import type { Order, UserModel } from "@/models/user.model";
+import { initializeApp } from "firebase/app";
+import { Timestamp, getDocs, writeBatch, type DocumentData, type DocumentSnapshot, type PartialWithFieldValue, type SetOptions, type Unsubscribe, Query, QuerySnapshot, Transaction, type TransactionOptions, collectionGroup, } from 'firebase/firestore'
+import {
+  Firestore,
+  getFirestore,
+  collection,
+  doc,
+  setDoc,
+  addDoc,
+  getDoc,
+  serverTimestamp,
+  CollectionReference, DocumentReference,
+  updateDoc,
+  onSnapshot,
+  FieldValue,
+  query,
+  deleteDoc,
+  runTransaction,
+  connectFirestoreEmulator,
+} from "firebase/firestore";
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDkAf6TexWWlmXaccIiy3BQ5iFBXEBAkxo",
+  authDomain: "my-lady-8b48f.firebaseapp.com",
+  projectId: "my-lady-8b48f",
+  storageBucket: "my-lady-8b48f.appspot.com",
+  messagingSenderId: "921457340252",
+  appId: "1:921457340252:web:4ed71b5b2844f0ca4e5b99"
+};
+
+const app = initializeApp(firebaseConfig);
+let p = runTransaction
+const instance: Firestore = getFirestore(app);
+
+
+export const firestore = {
+  instance,
+  connectFirestoreEmulator: () => connectFirestoreEmulator(instance, 'localhost', 8080),
+  Timestamp,
+  query,
+  collectionGroup: (path: string): Query<DocumentData> => collectionGroup(instance, path),
+  runTransaction: (updateFunction: (transaction: Transaction) => Promise<unknown>, options?: TransactionOptions): Promise<unknown> => runTransaction(instance, updateFunction, options),
+  writeBatch: () => writeBatch(instance),
+  onSnapshot: (pathOrDocRef: string | DocumentReference<DocumentData>, pathSegments: string[], callback: (doc: DocumentSnapshot) => void): Unsubscribe => onSnapshot(pathOrDocRef instanceof DocumentReference<DocumentData> ? pathOrDocRef : doc(instance, pathOrDocRef, ...pathSegments), callback),
+  collection: (path: string, ...pathSegments: string[]): CollectionReference => collection(instance, path, ...pathSegments),
+  doc: (path: string, ...pathSegments: string[]): DocumentReference<DocumentData> => doc(instance, path, ...pathSegments),
+  setDoc: (documentReference: DocumentReference<DocumentData>, data?: PartialWithFieldValue<DocumentData>, options?: SetOptions): Promise<void> => setDoc(documentReference, data),
+  async deleteDoc2(documentReference: DocumentReference<DocumentData>, data: PartialWithFieldValue<DocumentData>, options: SetOptions): Promise<string> {
+    const id = documentReference.id;
+    await deleteDoc(documentReference);
+
+    return id;
+  },
+  addDoc: (collectionReference: CollectionReference<unknown>, data: Partial<Order> | Partial<UserModel>): Promise<DocumentReference<Order> | DocumentReference<Order>> => addDoc(collectionReference, data) as Promise<DocumentReference<Order> | DocumentReference<Order>>,
+  getDoc: (documentReference: DocumentReference<unknown>): Promise<DocumentSnapshot<unknown>> => getDoc(documentReference),
+  getDocs: (query: Query<unknown>): Promise<QuerySnapshot<unknown>> => getDocs(query),
+  updateDoc: (documentReference: DocumentReference<unknown>, data: Partial<unknown>): Promise<void> => updateDoc(documentReference, data),
+  getServerTimestamp: (): FieldValue => serverTimestamp(),
+}
